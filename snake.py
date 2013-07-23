b2055ac7ca6c8d1db628eda510fe3cca87f42767
yiesheng.chua@gmail.com <yiesheng.chua@gmail.com>
2013-07-24 00:05:22 +1000

    Add food that randomly spawns on game area one at a time
    Snake can not grow after consuming food yet
    
    Game area of 12x12 grid (266x266px) is hardcoded, other sizes do not work
    
    Bug: Changing to the opposite direction will lose you the game

--- snake.py
+++ snake.py
@@ -11,13 +11,13 @@ WINDOWHEIGHT = 300
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Snake')
 
+# set game area
+gameArea = pygame.Rect(17, 17, 266, 266) # game area is set up as a 12x12 grid
+
 # set up colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
-
-# set game area
-gameArea = pygame.Rect(17, 17, 266, 266)
 
 # set up directions
 UP = 1
@@ -33,16 +33,42 @@ INITLENGTH = 3
 INITDIR = LEFT
 SNAKEMS = SNAKESIZE + 2
 
-# initialize snake
+def cloneList(someList):
+	clone = []
+	for item in someList:
+		clone.append(copy.copy(item))
+	return clone
+
+def createFood(x, y):
+	# create a food on 12x12 grid position x, y
+	gameAreaX = gameArea.left + 7 + 22 * (x - 1)
+	gameAreaY = gameArea.top + 7 + 22 * (y - 1)
+	return pygame.Rect(gameAreaX, gameAreaY, 10, 10)
+
+def createRandomFood(snakeBody):
+	# create a food on random location on game area
+	rerollFood = True
+	while rerollFood:
+		# this is to ensure no food is created under snake
+		rerollFood = False
+		foodItem = createFood(random.randint(1, 12), random.randint(1, 12))
+		for snakePart in snakeBody:
+			if snakePart.colliderect(foodItem):
+				rerollFood = True
+	return foodItem
+
 def reset():
-	global gameOver, snakeBody, snakeDir, snakeHead
+	# initialize snake
+	global gameOver, snakeBody, snakeDir, snakeHead, food
 	gameOver = False
 	snakeBody = []
+	food = []
 	snakeDir = INITDIR
 	snakeBody.append(pygame.Rect(STARTINGX, STARTINGY, SNAKESIZE, SNAKESIZE))
 	snakeHead = snakeBody[0]
 	for i in range(1, INITLENGTH):
 		snakeBody.append(pygame.Rect(STARTINGX + i * (SNAKESIZE + 2), STARTINGY, SNAKESIZE, SNAKESIZE))
+	food.append(createRandomFood(snakeBody))
 
 reset()
 
@@ -68,9 +94,7 @@ while True:
 
 	if not gameOver:
 		# store current copy of snake
-		snakeCopy = []
-		for snakePart in snakeBody:
-			snakeCopy.append(copy.copy(snakePart))
+		snakeClone = cloneList(snakeBody)
 
 		# movement
 		if snakeDir == UP:
@@ -81,10 +105,9 @@ while True:
 			snakeHead.right += SNAKEMS
 		elif snakeDir == LEFT:
 			snakeHead.right -= SNAKEMS
-
 		for i in range(len(snakeBody) - 1):
-			snakeBody[i + 1].x = snakeCopy[i].x
-			snakeBody[i + 1].y = snakeCopy[i].y
+			snakeBody[i + 1].x = snakeClone[i].x
+			snakeBody[i + 1].y = snakeClone[i].y
 
 		# collision detection
 		for i in range(len(snakeBody) - 1):
@@ -93,16 +116,25 @@ while True:
 				gameOver = True
 				print('Game over') # debug code
 				break
-
 		if not gameArea.collidepoint(snakeHead.center):
 			# collision with border
 			gameOver = True
 			print('Game over') # debug code
+		if snakeHead.colliderect(food[0]):
+			# collision with food
+			food.remove(food[0])
+
+		# create new food if previous was consumed
+		if not food:
+			food.append(createRandomFood(snakeBody))
 
 	if not gameOver:
-		# fill background
+		# draw background
 		windowSurface.fill(BLACK)
 		pygame.draw.rect(windowSurface, WHITE, gameArea, 1)
+
+		# draw food
+		pygame.draw.rect(windowSurface, WHITE, food[0])
 
 		# draw snake
 		for snakePart in snakeBody:
