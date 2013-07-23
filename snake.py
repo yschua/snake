2933e74653bf0c0a6d70daa02efd6e2d8cbe4a27
yiesheng.chua@gmail.com <yiesheng.chua@gmail.com>
2013-07-23 20:30:16 +1000

    Add area for snake to move in, with collision detection on borders
    Add reset game function, key: Esc

--- snake.py
+++ snake.py
@@ -5,9 +5,9 @@ from pygame.locals import *
 pygame.init()
 gameClock = pygame.time.Clock()
 
-# set up window
-WINDOWWIDTH = 400
-WINDOWHEIGHT = 400
+# set up main window
+WINDOWWIDTH = 300
+WINDOWHEIGHT = 300
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Snake')
 
@@ -16,29 +16,35 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 
+# set game area
+gameArea = pygame.Rect(17, 17, 266, 266)
+
 # set up directions
 UP = 1
 DOWN = 2
 LEFT = 3
 RIGHT = 4
-STOP = 0
 
 # set up snake
 SNAKESIZE = 20
-STARTINGX = 200
-STARTINGY = 200
-INITLENGTH = 10
+STARTINGX = gameArea.left + 178
+STARTINGY = gameArea.top + 134
+INITLENGTH = 3
 INITDIR = LEFT
 SNAKEMS = SNAKESIZE + 2
 
 # initialize snake
-gameOver = False
-snakeBody = []
-snakeDir = INITDIR
-snakeBody.append(pygame.Rect(STARTINGX, STARTINGY, SNAKESIZE, SNAKESIZE))
-snakeHead = snakeBody[0]
-for i in range(1, INITLENGTH):
-	snakeBody.append(pygame.Rect(STARTINGX + i * (SNAKESIZE + 2), STARTINGY, SNAKESIZE, SNAKESIZE))
+def reset():
+	global gameOver, snakeBody, snakeDir, snakeHead
+	gameOver = False
+	snakeBody = []
+	snakeDir = INITDIR
+	snakeBody.append(pygame.Rect(STARTINGX, STARTINGY, SNAKESIZE, SNAKESIZE))
+	snakeHead = snakeBody[0]
+	for i in range(1, INITLENGTH):
+		snakeBody.append(pygame.Rect(STARTINGX + i * (SNAKESIZE + 2), STARTINGY, SNAKESIZE, SNAKESIZE))
+
+reset()
 
 # game loop
 while True:
@@ -56,6 +62,9 @@ while True:
 				snakeDir = RIGHT
 			elif event.key == K_LEFT:
 				snakeDir = LEFT
+		if event.type == KEYUP:
+			if event.key == K_ESCAPE:
+				reset()
 
 	if not gameOver:
 		# store current copy of snake
@@ -79,14 +88,21 @@ while True:
 
 		# collision detection
 		for i in range(len(snakeBody) - 1):
+			# collision with snake body
 			if snakeHead.colliderect(snakeBody[i + 1]):
 				gameOver = True
-				print('Game over') # debug
+				print('Game over') # debug code
 				break
+
+		if not gameArea.collidepoint(snakeHead.center):
+			# collision with border
+			gameOver = True
+			print('Game over') # debug code
 
 	if not gameOver:
 		# fill background
 		windowSurface.fill(BLACK)
+		pygame.draw.rect(windowSurface, WHITE, gameArea, 1)
 
 		# draw snake
 		for snakePart in snakeBody:
